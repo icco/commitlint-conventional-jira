9736153c44c436780ce8c6e389a40843f3f65d4b
Nat Welch <nat@natwelch.com>
2022-07-04 20:18:55 +0000

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
-import CommitlintPlugin from "../@types"
+import { Commit, Plugin, RuleConfigCondition } from "@commitlint/types";
+import { ensureCase } from '@commitlint/ensure';
+import message from '@commitlint/message';
 
-export const commitlintConventionalJira: CommitlintPlugin = {
+const negated = (when?: string) => when === 'never';
+
+export const commitlintConventionalJira: Plugin = {
   rules: {
-    'jira-ticket': (parsed, when, value): (string | boolean)[] => {
-
-      return [false, 'Jira ticket is missing or not of the format JIRA-1234']
+    'subject-starts-jira': (parsed: Commit, when?: RuleConfigCondition | undefined, value?: undefined): [boolean, (string | undefined)?] => {
+      const subject = parsed.subject ?? "";
+      const matches = subject.match(/^(\([A-Z0-9]{2,6}-[0-9]{1,5})\): (.*)$/);
+      const valid = matches !== null && matches.length === 3;
+      return [negated(when) ? !valid : valid, 'Jira ticket is missing or not of the format JIRA-1234']
     },
+    'subject-case': (parsed: Commit, when?: RuleConfigCondition | undefined, value?: undefined): [boolean, (string | undefined)?] => {
+      const subject = parsed.subject ?? "";
+      const matches = subject.match(/^(\([A-Z0-9]{2,6}-[0-9]{1,5})\): (.*)$/);
+      const stripedSubject = matches?.[2] ?? "";
+
+      const checks = (Array.isArray(value) ? value : [value]).map((check) => {
+        if (typeof check === 'string') {
+          return {
+            when: 'always',
+            case: check,
+          };
+        }
+        return check;
+      });
+
+      const valid = checks.some((check) => {
+        if (check === undefined) {
+          return false;
+        }
+
+        const r = ensureCase(subject, check.case);
+        return negated(check.when) ? !r : r;
+      });
+
+      return [valid, message([`subject must`, negated(when) ? `not` : null, `be ${list}`]), }
   }
 }
 
